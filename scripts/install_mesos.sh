80b91b60749dfb09f7ec4f7d1393d1286e698813
ClassCat <support@classcat.com>
2015-09-14 21:21:23 +0900

    update

--- scripts/install_mesos.sh
+++ scripts/install_mesos.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+###############################################################
+# Copyright (C) 2015 ClassCat(R) Co.,Ltd. All rights reserved.
+###############################################################
+
+export LC_ALL=C
+
+. ../conf/mymesos.conf
+
+
 function init () {
   apt-get update
 }
 
+
 function install_jdk () {
   apt-get install -y openjdk-7-jdk
 }
+
 
 function install_mesos () {
   apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
@@ -18,8 +29,98 @@ function install_mesos () {
   apt-get -y install mesos marathon
 }
 
+
+function config_zookeeper () {
+  local ZOO_CONF_MYID=/etc/zookeeper/conf/myid
+  local ZOO_CONF_CFG=/etc/zookeeper/conf/zoo.cfg
+
+  echo "1" > ZOO_CONF_MYID
+
+  mv ZOO_CONF_CFG "${ZOO_CONF_CFG}.bak"
+
+  cat << _EOT_ > ZOO_CONF_CFG
+tickTime=2000
+initLimit=10
+syncLimit=5
+dataDir=/var/lib/zookeeper
+clientPort=2181
+_EOT_
+
+  echo "server.1=${ZOOKEEPER_IP}:2888:3888" >> ZOO_CONF_CFG
+}
+
+
+function config_mesos () {
+  echo "zk://${ZOOKEEPER_IP}:2181/mesos" > /etc/mesos/zk
+}
+
+
+function config_mesos_master () {
+  echo "1" > /etc/mesos-master/quorum
+}
+
+
+function restart_zookeeper () {
+  service zookeeper restart
+}
+
+
+function restart_mesos_master () {
+  service mesos-master restart
+}
+
+
+function restart_marathon () {
+  service marathon restart
+}
+
+
+function restart_mesos_slave () {
+  service mesos-slave restart
+}
+
+
+function disable_mesos_slave () {
+  service mesos-slave stop
+  sh -c "echo manual > /etc/init/mesos-slave.override"
+}
+
+
+function finalize () {
+  echo "http://${ZOOKEEPER_IP}:5050"
+}
+
+
+
+###################
+### ENTRY_POINT ###
+###################
+
 init
+
 install_jdk
+
 install_mesos
 
+config_zookeeper
+
+config_mesos
+
+config_mesos_master
+
+restart_zookeeper
+
+restart_mesos_master
+
+restart_marathon
+
+if [ $SLAVE_INSTALL == "true" ]; then
+  restart_mesos_slave
+else
+  disable_mesos_slave
+fi
+
 exit 0
+
+
+### End of Script ###
