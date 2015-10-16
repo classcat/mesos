aae96dbc4c3b392b509f8e7c2c67b2d178d4ecca
ClassCat <support@classcat.com>
2015-10-16 23:56:17 +0900

    drill

---
+++ scripts/install_drill.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+###############################################################
+# Copyright (C) 2015 ClassCat(R) Co.,Ltd. All rights reserved.
+###############################################################
+
+# --- HISOTRY -------------------------------------------------
+# -------------------------------------------------------------
+
+export LC_ALL=C
+
+
+# add spark user
+groupadd -g 502 drill
+
+useradd -u 502 -g drill -s /bin/bash -m drill
+
+
+mkdir -p /opt/packages
+
+cd /opt/packages
+
+wget http://getdrill.org/drill/download/apache-drill-1.1.0.tar.gz
+
+tar xfz apache-drill-1.1.0.tar.gz
+
+ln -s /opt/packages/apache-drill-1.1.0 /opt/drill
+
+#conf/drill-overrider.conf
+
+#drill.exec: {
+#  cluster-id: "drillbits1",
+#  zk.connect: "119.81.160.205:2181"
+#  #zk.connect: "localhost:2181"
+#}
+
+#./bin/drillbit.sh start
+
+echo 'unset JAVA_HOME' > /home/drill/.bash_profile
+echo 'export PATH=/usr/bin:$PATH' > /home/drill/.bash_profile
+
+echo 'export PATH=$PATH:/opt/drill/bin' > /home/drill/.bash_profile
+
+chown drill.drill /home/drill/.bash_profile
